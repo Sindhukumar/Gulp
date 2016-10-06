21061726f5f12ca9f99d96c09b6ff96cd9efcb11
anjukumar <anjukumar@github.com>
2016-10-06 14:54:27 -0400

    profile changes

---
+++ src/ProfileServlet.java
@@ -0,0 +1,80 @@
+
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Tool.Review;
+import Tool.Ruser;
+import Util.DbReview;
+import Util.DbUser;
+
+
+/**
+ * Servlet implementation class ProfileServlet
+ */
+@WebServlet("/ProfileServlet")
+public class ProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProfileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nextURL = "/Profile.jsp";
+		
+		String username = request.getParameter("username");
+		String useremail = request.getParameter("useremail");
+		String userpassword = request.getParameter("userpassword");
+		String zip = request.getParameter("zip");
+		
+		HttpSession session = request.getSession();
+		
+		Ruser user = (Ruser) session.getAttribute("user");
+		
+		user.setUsername(username);
+		user.setUseremail(useremail);
+		user.setUserpassword(userpassword);
+		user.setUserrole("user");
+		user.setZip(zip);
+		
+		DbUser.update(user);
+		List<Review> reviews = null;
+		session.setAttribute("user", user);
+
+		if (DbUser.isValidUser(useremail, userpassword)) {
+			user = DbUser.getUserByEmail(useremail);
+			session.setAttribute("user", user);
+			
+			reviews = DbReview.reviewofUser(user.getUserid());
+			session.setAttribute("reviews", reviews);
+		}
+
+		//getServletContext().getRequestDispatcher(nextURL).forward(request, response);
+		 response.sendRedirect(request.getContextPath() + nextURL);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
